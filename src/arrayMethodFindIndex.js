4d2e2acf6776184e747dc3fecb1fe9782016e628
gestap <gestap@gmail.com>
2019-04-15 18:21:22 +0300

    Solution

--- src/arrayMethodFindIndex.js
+++ src/arrayMethodFindIndex.js
@@ -5,7 +5,13 @@
  */
 function applyCustomFindIndex() {
   [].__proto__.findIndex2 = function(callback) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      let element = this[i];
+      if (callback(element, i, this)) {
+        return i;
+      }
+    }
+    return -1;
   };
 }
 

b899d0303f363cfddb1bb7c0b21606b087a1b26d
neitiris <veliantos@gmail.com>
2019-06-20 20:24:53 +0300

    solution commit

--- src/arrayMethodFindIndex.js
+++ src/arrayMethodFindIndex.js
@@ -6,6 +6,16 @@
 function applyCustomFindIndex() {
   [].__proto__.findIndex2 = function(callback) {
     // write code here
+    if (this.length > 0) {
+      for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+          return i;
+        }
+      }
+      return -1;
+    } else {
+      return -1;
+    }
   };
 }
 

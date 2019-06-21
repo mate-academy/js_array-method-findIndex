8129ce99640aac5e5cec9301083f7f900da02f69
IvanVFedenko <ivan.v.fedenko@gmail.com>
2019-06-21 14:24:15 +0300

    Soluton

--- src/arrayMethodFindIndex.js
+++ src/arrayMethodFindIndex.js
@@ -1,12 +1,16 @@
 'use strict';
-
 /**
  * Implement method FindIndex
  */
 function applyCustomFindIndex() {
   [].__proto__.findIndex2 = function(callback) {
-    // write code here
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        return i;
+      };
+      continue;
+    };
+    return -1;
   };
-}
-
+};
 module.exports = applyCustomFindIndex;

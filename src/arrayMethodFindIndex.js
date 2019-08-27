35a89b3b9c544e94230702bdd51da570348d0831
Miroslava <777shadowpriest777@gmail.com>
2019-08-27 17:59:41 +0300

    refactored

--- src/arrayMethodFindIndex.js
+++ src/arrayMethodFindIndex.js
@@ -5,20 +5,13 @@
  */
 function applyCustomFindIndex() {
   [].__proto__.findIndex2 = function(callback) {
-    if (this.length === 0) {
-      return -1;
-    }
-
-    let result = -1;
-
     for (let i = 0; i < this.length; i++) {
       if (callback(this[i], i, this)) {
-        result = i;
-        break;
+        return i;
       }
     }
 
-    return result;
+    return -1;
   };
 }
 

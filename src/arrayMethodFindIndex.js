60337a9c5b6bcc0ffe7db3570b7efe6880fbc405
KosBelozyorov <KosBelozyorov@gmail.com>
2019-08-27 18:10:17 +0300

    fix

--- src/arrayMethodFindIndex.js
+++ src/arrayMethodFindIndex.js
@@ -5,17 +5,13 @@
  */
 function applyCustomFindIndex() {
   [].__proto__.findIndex2 = function(callback) {
-    let result = -1;
-
     for (let i = 0; i < this.length; i += 1) {
       if (callback(this[i], i, this)) {
-        result = i;
-
-        return result;
+        return i;
       }
     }
 
-    return result;
+    return -1;
   };
 }
 

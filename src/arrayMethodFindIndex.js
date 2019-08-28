f63173b9f98bded06ac13563480f94b1f6e18c85
vladyslav223 <vladyslav223@gmail.com>
2019-08-28 19:07:00 +0300

    fixed

--- src/arrayMethodFindIndex.js
+++ src/arrayMethodFindIndex.js
@@ -7,10 +7,10 @@ function applyCustomFindIndex() {
   [].__proto__.findIndex2 = function(callback) {
     for (let i = 0; i < this.length; i++) {
       if (callback(this[i], i, this)) {
-
         return i;
       }
     }
+
     return -1;
   };
 }

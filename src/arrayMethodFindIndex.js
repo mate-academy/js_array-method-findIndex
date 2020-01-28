98a5ebce8663e4e0402fd6df547094a6e54d7764
aleksandrDnieper <aleksandr.dnieper@gmail.com>
2020-01-28 11:13:08 +0200

    removed if

--- src/arrayMethodFindIndex.js
+++ src/arrayMethodFindIndex.js
@@ -5,10 +5,6 @@
  */
 function applyCustomFindIndex() {
   [].__proto__.findIndex2 = function(callback) {
-    if (!arguments) {
-      return -1;
-    }
-
     for (let i = 0; i < this.length; i++) {
       if (callback(this[i], i, this)) {
         return i;

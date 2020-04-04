03d9068a08390e83477ca0ca44a2a4206947b57d
olenazubashenko <olenazubashenko@gmail.com>
2020-04-04 20:00:17 +0300

    task

--- src/arrayMethodFindIndex.js
+++ src/arrayMethodFindIndex.js
@@ -6,6 +6,14 @@
 function applyCustomFindIndex() {
   [].__proto__.findIndex2 = function(callback) {
     // write code here
+
+    for (let i = 0; i < this.length; i++) {
+      if ( callback(this[i], i, this)) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 

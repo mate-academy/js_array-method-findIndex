d43a8854bb6cbf61fc282bb520ed687e91516224
kate <kate@test.com>
2020-04-05 19:04:35 +0300

    solution

--- src/arrayMethodFindIndex.js
+++ src/arrayMethodFindIndex.js
@@ -6,6 +6,15 @@
 function applyCustomFindIndex() {
   [].__proto__.findIndex2 = function(callback) {
     // write code here
+    const arr = this;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (callback(arr[i], i, arr) === true) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 

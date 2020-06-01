ba4b2e44505d930655833ee6b40ade250e8ba9a4
Slava Polishchuk <dizerllc@gmail.com>
2020-06-01 16:43:52 +0300

    padding removed

--- src/arrayMethodFindIndex.js
+++ src/arrayMethodFindIndex.js
@@ -5,7 +5,6 @@
  */
 function applyCustomFindIndex() {
   [].__proto__.findIndex2 = function(callback) {
-
     for (let i = 0; i < this.length; i++) {
       if (callback(this[i], i, this)) {
         return i;

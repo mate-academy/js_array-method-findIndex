103feb34383d6cd23f12f4301a270c7359250bf8
YanG-6668 <goch.yannn@gmail.com>
2020-06-01 14:01:46 +0300

    findIndex

--- src/arrayMethodFindIndex.js
+++ src/arrayMethodFindIndex.js
@@ -5,7 +5,17 @@
  */
 function applyCustomFindIndex() {
   [].__proto__.findIndex2 = function(callback) {
-    // write code here
+    if (this.length === 0) {
+      return -1;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (callback(this[i], i, this)) {
+        return i;
+      }
+    }
+
+    return -1;
   };
 }
 
